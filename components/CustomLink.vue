2ee40cfd0626af6362d6acdd92034369bd09f071
Maickell Vilela <maickell@me.com>
2023-01-25 01:06:24 -0300

    External link styles

--- components/CustomLink.vue
+++ components/CustomLink.vue
@@ -2,11 +2,11 @@
   <a
     :href="to"
     class="custom-link"
-    :class="type"
+    :class="[type, external ? 'external' : '']"
     :target="[external ? '_blank' : '_self']"
   >
     <i class="icon" :class="icon"></i>
-    <span class=""><slot></slot></span>
+    <span class="label"><slot></slot></span>
   </a>
 </template>
 
@@ -35,25 +35,37 @@ export default {
 
 <style lang="scss" scoped>
 .custom-link {
-  color: red;
   @apply relative;
 
-  ::after {
+  .label {
+    @apply text-foreground transition-all duration-300 ease-in-out;
+  }
+
+  .label::before {
     content: "";
-    @apply absolute block w-[14px] h-[14px] text-accent top-0 -right-1 transform translate-x-full translate-y-3;
+    @apply absolute block w-0 h-[2px] bg-accent bottom-0 left-0 transform translate-y-1 transition-all duration-300 ease-in-out;
+  }
+
+  :hover {
+    @apply text-accent;
+
+    &.label::before {
+      @apply w-full;
+    }
+  }
+
+  &.external::after {
+    content: "";
+    @apply absolute block w-[14px] h-[14px] text-accent top-0 -right-1 transform translate-x-full translate-y-3 opacity-0 transition-all duration-300 ease-in-out;
 
     background-image: url("~/assets/images/external-link.svg");
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
-    opacity: 0;
-    transition: all 0.3s ease-in-out;
   }
-  :hover {
-    &::after {
-      opacity: 1;
-      @apply translate-x-full translate-y-0;
-    }
+
+  &.external:hover::after {
+    @apply translate-x-full translate-y-0 opacity-100;
   }
 }
 </style>
