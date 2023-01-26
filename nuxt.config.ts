f26d16978925dc833d8fc1c89fbf5ab8c5dbe5a9
Maickell Vilela <maickell@me.com>
2023-01-26 01:08:02 -0300

    gh-pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from '@nuxt/bridge'
+
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt'],
+  router: {
+    base: '/portfolio/' // base: '/<repository>/'
+  }
 })
 
