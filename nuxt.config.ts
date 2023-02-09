afeeeb46461bd590a3fa86cc2323da148f6ac0dc
Maickell Vilela <maickell@me.com>
2023-02-09 02:46:22 -0300

    buildAssetsDir

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ import { defineNuxtConfig } from '@nuxt/bridge'
 export default defineNuxtConfig({
   app: {
     baseURL: '/portfolio/', // base: '/<repository>/'
+    buildAssetsDir: 'assets',
   },
   ssr: false,
   modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt'],
