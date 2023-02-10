767eefaaca6fa4f7a65e4d00e7a23742aeb6daa0
Maickell Vilela <maickell@me.com>
2023-02-10 04:58:45 -0300

    google analytics

---
+++ plugins/vue-gtag.client.js
@@ -0,0 +1,10 @@
+import VueGtag, { trackRouter } from "vue-gtag-next";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(VueGtag, {
+    property: {
+      id: "G-DD7JM5Y65J",
+    },
+  });
+  trackRouter(useRouter());
+});
