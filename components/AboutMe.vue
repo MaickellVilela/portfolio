67ef9a5176f2e373d3c27d653919cfe599b54474
Maickell Vilela <maickell@me.com>
2023-02-12 15:03:37 -0300

    image decorator

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -2,12 +2,17 @@
   <Container id="aboutMeSection">
     <section class="py-16 md:py-32">
       <div class="flex gap-4 md:gap-8">
-        <img
-          class="rounded-lg w-1/2 md:w-1/4"
-          src="@/assets/images/MaickellVilelaProfilePicture.jpg"
-          alt=""
-        />
-        <div class="personal-bio">
+        <div class="relative w-1/2 md:w-1/4">
+          <img
+            class="rounded-lg w-full"
+            src="@/assets/images/MaickellVilelaProfilePicture.jpg"
+            alt=""
+          />
+          <span class="img-decorator a"></span>
+          <span class="img-decorator b"></span>
+          <span class="img-decorator c"></span>
+        </div>
+        <div class="personal-bio w-1/2 md:w-3/4">
           <h2 class="name text-4xl font-bold mb-2 md:mb-3">Maickell Vilela</h2>
           <p class="title text-accent text-sm">
             <span class="block md:inline-block mb-2 md:mb-0"
@@ -27,3 +32,22 @@
     </section>
   </Container>
 </template>
+
+<style lang="scss">
+.img-decorator {
+  @apply absolute block w-8 h-[6px] md:h-2 bg-accent rounded-sm;
+  filter: drop-shadow(0 0 8px theme("colors.accent" / 50%));
+
+  &.a {
+    @apply top-[15%] -left-2;
+  }
+
+  &.b {
+    @apply top-[75%] -left-4;
+  }
+
+  &.c {
+    @apply top-[25%] -right-2;
+  }
+}
+</style>
