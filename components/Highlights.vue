935957254f759f6870d358b248785b0541e2a256
Maickell Vilela <maickell@me.com>
2023-02-12 18:06:30 -0300

    highlight grid desktop

--- components/Highlights.vue
+++ components/Highlights.vue
@@ -1,17 +1,19 @@
 <template>
   <Container>
-    <section>
+    <section class="md:pb-32 pb-16">
       <h3 class="text-foreground opacity-60">Highlights</h3>
       <div class="cards-grid">
         <div class="card card-style1 design-xp center-content">
           <div class="content">
             <h4
-              class="text-darkBackground font-extrabold text-center leading-tight italic text-[44px]"
+              class="text-darkBackground font-extrabold text-center leading-tight italic text-[44px] md:text-[52px]"
             >
               15+
-              <span class="text-sm font-bold -ml-4">years</span>
+              <span class="text-sm md:text-base font-bold -ml-4">years</span>
             </h4>
-            <p class="text-darkBackground font-light text-sm text-center">
+            <p
+              class="text-darkBackground font-light text-sm md:text-base md:leading-none whitespace-nowrap text-center"
+            >
               as a UX/UI Designer
             </p>
           </div>
@@ -21,7 +23,9 @@
             <h4 class="text-foreground opacity-60 font-bold italic text-sm">
               Top Industries
             </h4>
-            <ol class="text-accent text-sm leading-6 list-disc ml-5">
+            <ol
+              class="text-accent text-sm md:text-base leading-6 md:leading-7 md:mt-2 list-disc ml-5"
+            >
               <li>Food Delivery</li>
               <li>Telecom</li>
               <li>Real Estate</li>
@@ -35,16 +39,20 @@
         <div class="card card-style3 projects center-content">
           <div class="content">
             <h4
-              class="text-accent font-extrabold text-center leading-tight italic text-[44px]"
+              class="text-accent font-extrabold text-center leading-tight italic text-[44px] md:text-[52px]"
             >
               40+
             </h4>
-            <p class="text-accent font-light text-sm text-center">projects</p>
+            <p
+              class="text-accent font-light text-sm md:text-base md:leading-none whitespace-nowrap text-center"
+            >
+              projects
+            </p>
           </div>
         </div>
         <div class="card card-style3 quote1 relative">
           <div class="content">
-            <p class="text-accent serif-font italic text-base">
+            <p class="text-accent serif-font italic text-base md:text-lg">
               “Continuous attention to technical excellence and good design
               enhances agility.”
             </p>
@@ -59,7 +67,9 @@
             <h4 class="text-foreground opacity-60 font-bold italic text-sm">
               Technologies
             </h4>
-            <ol class="text-accent text-sm leading-6 list-disc ml-5">
+            <ol
+              class="text-accent text-sm md:text-base leading-6 md:leading-7 md:mt-2 list-disc ml-5"
+            >
               <li>Vue.js</li>
               <li>Tailwind CSS</li>
               <li>Nuxt</li>
@@ -73,12 +83,14 @@
         <div class="card card-style1 front-xp center-content">
           <div class="content">
             <h4
-              class="text-darkBackground font-extrabold text-center leading-tight italic text-[44px]"
+              class="text-darkBackground font-extrabold text-center leading-tight italic text-[44px] md:text-[52px]"
             >
               8+
-              <span class="text-sm font-bold -ml-4">years</span>
+              <span class="text-sm md:text-base font-bold -ml-4">years</span>
             </h4>
-            <p class="text-darkBackground font-light text-sm text-center">
+            <p
+              class="text-darkBackground font-light text-sm md:text-base md:leading-none whitespace-nowrap text-center"
+            >
               as a frontend dev
             </p>
           </div>
@@ -93,10 +105,10 @@
         </div>
         <div class="card card-style1 learning center-content">
           <div class="content">
-            <h4 class="text-darkBackground font-bold text-sm">
+            <h4 class="text-darkBackground font-bold text-sm md:text-base">
               Always Learning
             </h4>
-            <p class="text-darkBackground text-xs">
+            <p class="text-darkBackground text-xs md:text-sm">
               Practicing Blender 3D <br />
               Learning iOS Dev. <br />
               Reading Philosophy
@@ -105,7 +117,9 @@
         </div>
         <div class="card card-style3 quote2 relative">
           <div class="content">
-            <p class="text-accent serif-font italic text-base leading-snug">
+            <p
+              class="text-accent serif-font italic text-base md:text-lg leading-snug"
+            >
               “Our highest priority is to satisfy the customer through early and
               continuous delivery of valuable software.”
             </p>
@@ -117,18 +131,20 @@
         </div>
         <div class="card card-style2 design-system center-content">
           <div class="content">
-            <h4 class="text-accent font-bold text-sm">Design System</h4>
-            <p class="text-accent text-xs">
+            <h4 class="text-accent font-bold text-sm md:text-base">
+              Design System
+            </h4>
+            <p class="text-accent text-xs md:text-sm">
               specialist. 10+ design systems built from scratch
             </p>
           </div>
         </div>
         <div class="card card-style1 volunteer center-content">
           <div class="content">
-            <h4 class="text-darkBackground font-bold text-sm">
+            <h4 class="text-darkBackground font-bold text-sm md:text-base">
               Volunteer Work
             </h4>
-            <p class="text-darkBackground text-xs">
+            <p class="text-darkBackground text-xs md:text-sm">
               Mentoring for startups and students
             </p>
           </div>
@@ -184,8 +200,18 @@
   grid-gap: 1rem;
   margin-top: 1rem;
 
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: repeat(4, 1fr);
+    grid-template-areas:
+      "design-xp industries quote1 quote1"
+      "projects industries front-xp technologies"
+      "quote2 quote2 figma-course technologies"
+      "volunteer design-system figma-course learning";
+  }
+
   .card {
-    @apply rounded-lg p-2;
+    @apply rounded-lg p-2 md:p-4;
 
     &.center-content {
       @apply flex justify-center items-center;
