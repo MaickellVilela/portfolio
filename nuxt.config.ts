1c250b0b84877498ab22bb5ab870fd57560e39a3
Maickell Vilela <maickell@me.com>
2023-02-15 00:44:16 -0300

    meta description and favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,9 +6,16 @@ const GENERAL_DESCRIPTION =
   "I am a UI/UX designer with a dedication to creating intuitive and aesthetically pleasing user experiences. With a deep understanding of Design Systems and hands-on experience with Figma, I bring my designs to life using modern frontend technologies such as Vue.js, Tailwind CSS, Nuxt, and HTML/CSS. My attention to detail extends to the quality assurance process, where I use tools like Cypress and Jest to ensure the final product meets high standards. Whether it's in Brazilian Portuguese, English, Spanish or French, my goal is to create solutions that deliver value to users and the business. Let's work together!";
 const KEYWORDS =
   "frontend developer, agile designer, design system, Figma, Vue.js, Tailwind CSS, Nuxt, Cypress, Jest, HTML, CSS, JavaScript";
-const OG_IMAGE_URL =
-  "https://maickellvilela.github.io/portfolio/public/MaickellVilela-cover.jpg";
 const WEBSITE_URL = "https://maickellvilela.github.io/portfolio/";
+
+const OG_IMAGE = WEBSITE_URL + "MaickellVilela-cover.jpg";
+const BROWSER_CONFIG = WEBSITE_URL + "browserconfig.xml";
+const APPLE_ICON = WEBSITE_URL + "apple-touch-icon.png";
+const ICON_32 = WEBSITE_URL + "favicon-32x32.png";
+const ICON_16 = WEBSITE_URL + "favicon-16x16.png";
+const MANIFEST = WEBSITE_URL + "site.webmanifest";
+const MASK_ICON = WEBSITE_URL + "safari-pinned-tab.svg";
+const FAVICON = WEBSITE_URL + "favicon.ico";
 
 export default defineNuxtConfig({
   app: {
@@ -41,7 +48,7 @@ export default defineNuxtConfig({
         { property: "og:url", content: WEBSITE_URL },
         {
           property: "og:image",
-          content: OG_IMAGE_URL,
+          content: OG_IMAGE,
         },
         { property: "og:image:width", content: "1200" },
         { property: "og:image:height", content: "630" },
@@ -56,7 +63,7 @@ export default defineNuxtConfig({
         },
         {
           name: "twitter:image",
-          content: OG_IMAGE_URL,
+          content: OG_IMAGE,
         },
         { property: "og:locale", content: "en_US" },
         { property: "og:locale:alternate", content: "pt_BR" },
@@ -65,8 +72,7 @@ export default defineNuxtConfig({
         { name: "msapplication-TileColor", content: "#050e1c" },
         {
           name: "msapplication-config",
-          content:
-            "https://maickellvilela.github.io/portfolio/public/browserconfig.xml",
+          content: BROWSER_CONFIG,
         },
         { name: "theme-color", content: "#050E1C" },
       ],
@@ -74,32 +80,32 @@ export default defineNuxtConfig({
         {
           rel: "apple-touch-icon",
           sizes: "180x180",
-          href: "https://maickellvilela.github.io/portfolio/public/apple-touch-icon.png",
+          href: APPLE_ICON,
         },
         {
           rel: "icon",
           type: "image/png",
           sizes: "32x32",
-          href: "https://maickellvilela.github.io/portfolio/public/favicon-32x32.png",
+          href: ICON_32,
         },
         {
           rel: "icon",
           type: "image/png",
           sizes: "16x16",
-          href: "https://maickellvilela.github.io/portfolio/public/favicon-16x16.png",
+          href: ICON_16,
         },
         {
           rel: "manifest",
-          href: "https://maickellvilela.github.io/portfolio/public/site.webmanifest",
+          href: MANIFEST,
         },
         {
           rel: "mask-icon",
-          href: "https://maickellvilela.github.io/portfolio/public/safari-pinned-tab.svg",
+          href: MASK_ICON,
           color: "#050e1c",
         },
         {
           rel: "shortcut icon",
-          href: "https://maickellvilela.github.io/portfolio/public/favicon.ico",
+          href: FAVICON,
         },
       ],
     },
